aa6e582c0d02f466d6a68c0b93fd09160b9d8139
Debabrata24 <mr.debabrtapc2006@gmail.com>
2025-10-17 01:02:10 +0530

    oth

--- server/index.ts
+++ server/index.ts
@@ -56,18 +56,6 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  // const port = parseInt(process.env.PORT || '5000', 10);
-  // server.listen({
-  //   port,
-  //   host: "5000",
-  //   reusePort: true,
-  // }, () => {
-  //   log(`serving on port ${5000}`);
-  // });
 
 app.listen(5001, () => {
   console.log(`✅ Server running at http://localhost:${5001}`);
